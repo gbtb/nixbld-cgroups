9251547e5d14a475f577275926aac739cc402366
gbtb <goodbetterthebeast3@gmail.com>
2023-02-22 22:11:59 +1000

    Merged config.nix into module options

--- default.nix
+++ default.nix
@@ -12,19 +12,18 @@ let
     echo $n > $out/value.nix
   '';
   nproc = import "${nproc_}/value.nix";
-  default-config = import ./config.nix { inherit nproc; };
-  profiles = attrNames default-config.profiles;
-  profilesJson = pkgs.writeText "nixbld-cgroups-profiles" (
+  slicesNames = attrNames cfg.slices;
+  slicesJson = pkgs.writeText "nixbld-cgroups-slices" (
     let
-      preparedProfiles =
+      preparedSlices =
         mapAttrs
           (name: attrs:
             let props = mapAttrs (name: value: name + "=" + (toString value)) attrs; in
             concatStringsSep " " (attrValues props)
           )
-          default-config.profiles;
+          cfg.slices;
     in
-    toJSON preparedProfiles
+    toJSON preparedSlices
   );
   description = "Tool for simple resource limiting of nix builds with help of cgroups";
   nixbld-cgroups = pkgs.writeShellApplication {
@@ -49,11 +48,11 @@ let
 
       EOF
       }
-                        availableProfiles="${builtins.toString profiles}"
-                        #cat "${profilesJson}"
-                        echo "Available profiles: [$availableProfiles]"
+                        availableSlices="${builtins.toString slicesNames}"
+                        #cat "${slicesJson}"
+                        echo "Available slices: [$availableSlices]"
                         cpus=""
-                        profile=""
+                        slice=""
                         shopt -s extglob
                         while :; do
                             case ''${1-default} in
@@ -69,12 +68,12 @@ let
                                       fail 'ERROR: "--cpus" requires a non-empty argument with number of cpus.';
                                   fi
                                   ;;
-                                -p|--profile)
+                                -p|--slice)
                                   if [[ $2 == +([a-zA-Z0-9]) ]]; then
-                                    profile=$2
+                                    slice=$2
                                     shift 1;
                                   else
-                                      fail 'ERROR: "--profile" requires a non-empty argument with a name of the profile';
+                                      fail 'ERROR: "--slice" requires a non-empty argument with a name of the slice';
                                   fi
                                   ;;
                                 *)               # Default case: No more options, so break out of the loop.
@@ -82,9 +81,9 @@ let
                             esac
                             shift
                         done
-                      echo "CPU: $cpus, Profile: ''${profile:-Not set}"
-                      if [[ "$cpus" != "" ]] && [[ $profile != "" ]]; then
-                        error "You can use either profile or the number of cpus as an argument";
+                      echo "CPU: $cpus, Slice: ''${slice:-Not set}"
+                      if [[ "$cpus" != "" ]] && [[ $slice != "" ]]; then
+                        error "You can use name of the slice or the number of cpus as an argument";
                       fi
                       nproc=$(nproc)
                       if [[ $cpus -ge $nproc ]]; then
@@ -97,43 +96,59 @@ let
                         exit 0
                       fi
 
-                      if [[ "$profile" != "" ]]; then
-                        profileJson=$(jq -r ."$profile" < ${profilesJson})
-                        if [[ "$profileJson" == "null" ]]; then
-                          error "Profile with name $profile wasn't configured"
+                      if [[ "$slice" != "" ]]; then
+                        sliceJson=$(jq -r ."$slice" < ${slicesJson})
+                        if [[ "$sliceJson" == "null" ]]; then
+                          error "Slice with the name $slice wasn't configured"
                           exit 1
                         fi
 
-                        echo "${profilesJson}"
-                        echo "$profileJson"
-                        a="systemctl set-property --runtime nix-daemon.service $profileJson"
+                        echo "${slicesJson}"
+                        echo "$sliceJson"
+                        a="systemctl set-property --runtime nix-daemon.service $sliceJson"
                         $a
                       fi
 
     '';
   };
-  systemdScript = pkgs.writeShellScript "nixbld-dbus-monitor" 
-  ''dbus-monitor "type='signal',path=/org/freedesktop/ScreenSaver" | sed -u -n -e "s/   boolean true/yandex-disk start/p" -e "s/   boolean false/yandex-disk end/p"'';
+  systemdScript = pkgs.writeShellScript "nixbld-dbus-monitor"
+    ''dbus-monitor "type='signal',path=/org/freedesktop/ScreenSaver" | sed -u -n -e "s/   boolean true/yandex-disk start/p" -e "s/   boolean false/yandex-disk end/p"'';
 in
-{
-  options = {
-    services.nixbld-dbus-monitor = {
-      enable = mkEnableOption "dbus-monitor watching for ScreenSaver";
-      lockscreenProfile = mkOption {
-        type = types.enum profiles;
-        default = builtins.head profiles;
-        example = "";
-        description = "";
+rec {
+  lib = {
+    share = a: b: builtins.floor (a * b);
+    toAllowedCpus = x: "0-${toString x}";
+  };
+  module = {
+    options = {
+      services.nixbld-dbus-monitor = {
+        enable = mkEnableOption "dbus-monitor watching for ScreenSaver";
+        slices = mkOption {
+          description = "Definition of systemd slices, available for the nixbld-cgroups tool";
+          type = types.attrsOf types.anything;
+          default = {
+            allCores = { AllowedCPUs = lib.toAllowedCpus (nproc - 1); };
+            browsing = { AllowedCPUs = lib.toAllowedCpus (lib.share nproc 0.7); };
+            work = { AllowedCPUs = lib.toAllowedCpus (lib.share nproc 0.5); };
+            gaming = { AllowedCPUs = lib.toAllowedCpus (lib.share nproc 0.2); };
+          };
+        };
+        lockscreenProfile = mkOption {
+          type = types.enum (attrNames cfg.slices);
+          default = builtins.head (attrNames cfg.slices);
+          example = "";
+          description = "";
+        };
       };
     };
-  };
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ nixbld-cgroups ];
-    systemd.user.services.nixbld-dbus-monitor = {
-      wantedBy = [ "multi-user.target" ];
-      #environment = { DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${uid}/bus" };
-      path = [ pkgs.dbus nixbld-cgroups ];
-      serviceConfig.ExecStart = systemdScript;
+    config = mkIf cfg.enable {
+      environment.systemPackages = [ nixbld-cgroups ];
+      systemd.user.services.nixbld-dbus-monitor = {
+        wantedBy = [ "multi-user.target" ];
+        #environment = { DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${uid}/bus" };
+        path = [ pkgs.dbus nixbld-cgroups ];
+        serviceConfig.ExecStart = systemdScript;
+      };
     };
   };
 }
